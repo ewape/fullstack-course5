cb9edb8174fe6f475c4a3d1183d2afc9bcb2d523
Ewa <ewapettke@gmail.com>
2016-09-21 21:46:45 +0200

    add assignment 2 solution

--- examples/Lecture12/app.js
+++ examples/Lecture12/app.js
@@ -2,13 +2,17 @@
 'use strict';
 
 angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+.controller('MsgController', MsgController)
+.filter('custom', customFilter)
+.filter('loves', lovesFilter)
+.filter('truth', truthFilter);
 
-MsgController.$inject = ['$scope', '$filter'];
-function MsgController($scope, $filter) {
+MsgController.$inject = ['$scope', '$filter', 'customFilter', 'lovesFilter', 'truthFilter'];
+
+function MsgController($scope, $filter, customFilter, lovesFilter, truthFilter) {
   $scope.name = "Yaakov";
   $scope.stateOfBeing = "hungry";
-  $scope.cookieCost = .45;
+  $scope.cookieCost = 0.45;
 
   $scope.sayMessage = function () {
     var msg = "Yaakov likes to eat healthy snacks at night!";
@@ -16,8 +20,44 @@ function MsgController($scope, $filter) {
     return output;
   };
 
+  $scope.sayLovesMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    var output = lovesFilter(msg);
+    return output;
+  };
+
+  $scope.sayTruthMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    var output = truthFilter(msg, "healthy", "unhealthy");
+    return output;
+  };
+
   $scope.feedYaakov = function () {
     $scope.stateOfBeing = "fed";
+  };
+
+  $scope.filtered = customFilter($scope.cookieCost);
+}
+
+function customFilter() {
+  return function(value) {
+    return  value * 5;
+  };
+}
+
+function lovesFilter() {
+  return function(input) {
+    input = input || '';
+    input = input.replace("likes", "loves");
+    return input;
+  };
+}
+
+function truthFilter() {
+  return function(input, target, replace) {
+    input = input || '';
+    input = input.replace(target, replace);
+    return input;
   };
 }
 
